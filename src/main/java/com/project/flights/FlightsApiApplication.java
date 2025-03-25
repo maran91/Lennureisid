69880c02072ea24baa1b528578d56b2b037969a3
maran91 <Margus.Andrei@gmail.com>
2025-03-25 17:38:34 +0200

    Initial commit

---
+++ src/main/java/com/project/flights/FlightsApiApplication.java
@@ -0,0 +1,15 @@
+package com.project.flights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FlightsApiApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightsApiApplication.class, args);
+    }
+
+}
