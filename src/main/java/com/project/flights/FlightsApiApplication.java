3848772b6ed8b6f06995cefe56735dc625cb6e8c
maran91 <Margus.Andrei@gmail.com>
2025-03-31 23:13:32 +0300

    Add CORS configuration to allow frontend access

--- src/main/java/com/project/flights/FlightsApiApplication.java
+++ src/main/java/com/project/flights/FlightsApiApplication.java
@@ -2,11 +2,23 @@ package com.project.flights;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FlightsApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(FlightsApiApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 }
